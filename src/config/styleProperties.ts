e29d91cf9f60b2f21dd8ae296f21ca0c9d39d279
oskar <36141470+devskar@users.noreply.github.com>
2022-01-19 08:19:00 +0100

    adding styleProperties

---
+++ src/config/styleProperties.ts
@@ -0,0 +1,6 @@
+export default {
+  primary_background_color: 'rgb(114, 120, 141)',
+  secondary_background_color: 'rgb(114, 120, 160)',
+  primary_highlight_color: 'rgb(198, 201, 210)',
+  secondary_highlight_color: 'rgb(244, 244, 246)',
+};
